9292c59a74ecab1de9ea0d73de57c9f9b2d81dcd
hst1189 <hst1189@hotmail.com>
2024-10-25 20:03:36 +0900

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,10 +1,6 @@
 try {
-    $("<link>").attr({
-        href: "//www.grapehut.us.kg/live2d-widget/assets/waifu.min.css",
-        rel: "stylesheet",
-        type: "text/css"
-    }).appendTo('head');
-    $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+    $("<link>").attr({href: "//www.grapehut.us.kg/live2d-widget/assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({
         url: '//www.grapehut.us.kg/live2d-widget/assets/waifu-tips.min.js',
         dataType: "script",
@@ -32,4 +28,5 @@ try {
     });
 } catch (err) {
     console.log('[Error] JQuery is not defined.')
+    alert('[Error] JQuery is not defined.')
 }
