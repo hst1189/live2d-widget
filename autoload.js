7538952d24def1b2c7abde833ee270e298620a93
hst1189 <hst1189@hotmail.com>
2024-10-25 15:19:45 +0900

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 try {
-    $("<link>").attr({href: "assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "//www.grapehut.us.kg/live2d-widget/assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({url: '//www.grapehut.us.kg/live2d-widget/assets/waifu-tips.min.js', dataType:"script", cache: true, success: function() {
         $.ajax({url: '//www.grapehut.us.kg/live2d-widget/assets/live2d.min.js', dataType:"script", cache: true, success: function() {
