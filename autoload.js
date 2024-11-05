9b42dee92f88ddeec422aec4a3335f8b2cf3a72b
Your Name <you@example.com>
2024-11-06 00:46:38 +0900

    change

--- autoload.js
+++ autoload.js
@@ -1,18 +1,18 @@
 try {
-    $("<link>").attr({href: "//www.grapehut.us.kg/live2d-widget/assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: "//cdn.jsdelivr.net/gh/hst1189/live2d-widget/assets/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"> <span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({
-        url: '//www.grapehut.us.kg/live2d-widget/assets/ModelDefine.js',
+        url: '//cdn.jsdelivr.net/gh/hst1189/live2d-widget/ModelDefine.js',
         dataType: "script",
         cache: false,
         success: function() {
             $.ajax({
-                url: '//www.grapehut.us.kg/live2d-widget/assets/waifu-tips.min.js',
+                url: '//cdn.jsdelivr.net/gh/hst1189/live2d-widget/assets/waifu-tips.min.js',
                 dataType: "script",
                 cache: false,
                 success: function() {
                     $.ajax({
-                        url: '//www.grapehut.us.kg/live2d-widget/assets/live2d.min.js',
+                        url: '//cdn.jsdelivr.net/gh/hst1189/live2d-widget/assets/live2d.min.js',
                         dataType: "script",
                         cache: false,
                         success: function() {
@@ -22,7 +22,7 @@ try {
                             // live2d_settings['modelTexturesId'] = 0;
                         
                             /* 初始化*/ 
-                            initModel('//www.grapehut.us.kg/live2d-widget/assets/waifu-tips.json');
+                            initModel('//cdn.jsdelivr.net/gh/hst1189/live2d-widget/assets/waifu-tips.json');
                         }
                     });
                 }
